f739bdde3df56a3af6f32d18377e4b63f621e904
xotnet <150250186+xotnet@users.noreply.github.com>
2025-08-31 13:20:05 +0000

    Create ipshield.1bt.uk.js

---
+++ ipshield.1bt.uk.js
@@ -0,0 +1,11 @@
+addSubDomain({
+  description: 'A digital currency project with zero fees and ease of use', // describe your project in this field
+  domain: '1bt.uk', // aka "root-domain". select between '1bt.uk' and 'is-an.app'
+  subdomain: 'ipshield', // desired subdomain name
+  owner: {
+    repo: 'https://github.com/xotnet/domains',
+    email: 'root@notcp.lol',
+  },
+  record: {
+    NS: ['ns1.desec.io', 'ns2.desec.org'],
+  },
